3d8b3239600f3c6e6496f4d4820495286f5ef5f0
Fodé Guirassy <fode.guirassy@ekino.com>
2018-11-20 10:46:25 +0100

    home page

--- stopit/src/app/app.component.ts
+++ stopit/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {element} from "protractor";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [  "../../node_modules/bootstrap/dist/css/bootstrap.css",
+    './app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  userEmail : string
+  userPassword : string
+  errorMessage : string
+
+  ngOnInit(): void {}
+
+  onLoginClicked() {
+
+    console.log(this.userEmail)
+    console.log(this.userPassword)
+
+    this.errorMessage = "You're wrong"
+
+  }
+
 }
